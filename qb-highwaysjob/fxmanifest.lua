21f4dd718cd382cc1f37594b2771768c115a0aad
Slim Shady <162316192+ShadyScripts@users.noreply.github.com>
2024-03-05 12:12:27 +0000

    Add files via upload

---
+++ qb-highwaysjob/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'qb-HighwaysJob by Slim Shady'
+version '1.0.0'
+made_by 'Slim Shady'
+
+
+shared_script 'config.lua'
+
+client_scripts {
+	'client/main.lua',
+	'client/job.lua',
+}
+
+server_script 'server/main.lua'
+
+lua54 'yes'
+
+dependancy {
+	'qb-motorwaymaintence',
+	'qb-highways',
+}
